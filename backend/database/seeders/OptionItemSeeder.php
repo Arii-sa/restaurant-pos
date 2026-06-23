5457f7fb1c9c8dbff2b50a9b12bdf3af53b1919a
arii naru <nakamuraarisa1102@icloud.com>
2026-06-23 14:09:15 +0900

    seeder修正

--- backend/database/seeders/OptionItemSeeder.php
+++ backend/database/seeders/OptionItemSeeder.php
@@ -12,78 +12,82 @@ class OptionItemSeeder extends Seeder
     {
         // ピクルスグループ
         $pickleGroups = OptionGroup::where('name', 'ピクルス')->get();
+
         foreach ($pickleGroups as $group) {
-            OptionItem::create([
-                'option_group_id' => $group->id,
-                'name'            => 'あり',
-                'price_diff'      => 0,
-                'is_default'      => true,
-            ]);
-            OptionItem::create([
-                'option_group_id' => $group->id,
-                'name'            => 'なし',
-                'price_diff'      => 0,
-                'is_default'      => false,
-            ]);
+
+            OptionItem::updateOrCreate(
+                [
+                    'option_group_id' => $group->id,
+                    'name'            => 'あり',
+                ],
+                [
+                    'price_diff' => 0,
+                    'is_default' => true,
+                ]
+            );
+
+            OptionItem::updateOrCreate(
+                [
+                    'option_group_id' => $group->id,
+                    'name'            => 'なし',
+                ],
+                [
+                    'price_diff' => 0,
+                    'is_default' => false,
+                ]
+            );
         }
 
         // ソースグループ
         $sauceGroups = OptionGroup::where('name', 'ソース')->get();
+
         foreach ($sauceGroups as $group) {
-            OptionItem::create([
-                'option_group_id' => $group->id,
-                'name'            => 'マスタード',
-                'price_diff'      => 0,
-                'is_default'      => true,
-            ]);
-            OptionItem::create([
-                'option_group_id' => $group->id,
-                'name'            => 'ケチャップ',
-                'price_diff'      => 0,
-                'is_default'      => false,
-            ]);
-            OptionItem::create([
-                'option_group_id' => $group->id,
-                'name'            => 'バーベキュー',
-                'price_diff'      => 50,
-                'is_default'      => false,
-            ]);
-            OptionItem::create([
-                'option_group_id' => $group->id,
-                'name'            => 'テリヤキ',
-                'price_diff'      => 50,
-                'is_default'      => false,
-            ]);
+
+            $sauces = [
+                ['name' => 'マスタード', 'price_diff' => 0, 'is_default' => true],
+                ['name' => 'ケチャップ', 'price_diff' => 0, 'is_default' => false],
+                ['name' => 'バーベキュー', 'price_diff' => 50, 'is_default' => false],
+                ['name' => 'テリヤキ', 'price_diff' => 50, 'is_default' => false],
+            ];
+
+            foreach ($sauces as $sauce) {
+                OptionItem::updateOrCreate(
+                    [
+                        'option_group_id' => $group->id,
+                        'name'            => $sauce['name'],
+                    ],
+                    [
+                        'price_diff' => $sauce['price_diff'],
+                        'is_default' => $sauce['is_default'],
+                    ]
+                );
+            }
         }
 
         // トッピンググループ
         $toppingGroups = OptionGroup::where('name', 'トッピング')->get();
+
         foreach ($toppingGroups as $group) {
-            OptionItem::create([
-                'option_group_id' => $group->id,
-                'name'            => 'チーズ追加',
-                'price_diff'      => 100,
-                'is_default'      => false,
-            ]);
-            OptionItem::create([
-                'option_group_id' => $group->id,
-                'name'            => 'ベーコン追加',
-                'price_diff'      => 150,
-                'is_default'      => false,
-            ]);
-            OptionItem::create([
-                'option_group_id' => $group->id,
-                'name'            => 'アボカド追加',
-                'price_diff'      => 120,
-                'is_default'      => false,
-            ]);
-            OptionItem::create([
-                'option_group_id' => $group->id,
-                'name'            => 'エッグ追加',
-                'price_diff'      => 100,
-                'is_default'      => false,
-            ]);
+
+            $toppings = [
+                ['name' => 'チーズ追加', 'price_diff' => 100],
+                ['name' => 'ベーコン追加', 'price_diff' => 150],
+                ['name' => 'アボカド追加', 'price_diff' => 120],
+                ['name' => 'エッグ追加', 'price_diff' => 100],
+            ];
+
+            foreach ($toppings as $topping) {
+                OptionItem::updateOrCreate(
+                    [
+                        'option_group_id' => $group->id,
+                        'name'            => $topping['name'],
+                    ],
+                    [
+                        'price_diff' => $topping['price_diff'],
+                        'is_default' => false,
+                    ]
+                );
+            }
         }
     }
 }
-
